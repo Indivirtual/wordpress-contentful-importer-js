43a8a9cf732481b3ba8d4b1ea4e6b51be443d7b8
Kah Tang <rainmaker2k@gmail.com>
2018-09-12 09:36:47 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 let config = {
-    accessToken: 'CFPAT-16fa6024747f5b4bbbdae94b129790b2279a55adecd9a488e480bc71e20bc659',
-    spaceId: 'tmk3oi6u1mmf',
+    accessToken: 'ACCESS-TOKEN',
+    spaceId: 'SPACEID',
     environment: 'master'
 }
 module.exports = config;
